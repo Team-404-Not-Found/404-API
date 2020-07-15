0ca78f22ab5cc89975061a7fa76fcd41529559d9
Compenso <wesurbaniakmusic@gmail.com>
2020-07-15 10:04:30 -0600

    Curl Scripts for product completed but not tested
    Need to build the product route and test the curl Scripts

---
+++ curl-scripts/product/update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/products"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+--header "Authorization: Bearer ${TOKEN}" \
+--data '{
+  "product": {
+    "name": "'"${NAME}"'",
+    "price": "'"${PRICE}"'",
+    "description": "'"${DESCRIPTION}"'",
+    "imageUrl": "'"${IMAGEURL}"'",
+    "category": "'"${CATEGORY}"'",
+    "inStock": "'"${INSTOCK}"'",
+    "quantity": "'"${QUANTITY}"'",
+    "seller": "'"${SELLER}"'",
+  }
+}'
+
+echo
