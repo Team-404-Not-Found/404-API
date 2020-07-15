d9e67ebe535265b24c425367372fb7b928aae9b6
Nuanjan <Maine09102016>
2020-07-15 16:50:53 -0400

    finish cart models still working on cart routes
    created cart model and subdocument of products array
    create cart routes
    curl-script cart create.sh

---
+++ curl-scripts/cart/create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/cart"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+    --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "cart":{
+          "owner": "'"${OWNER}"'"
+    }
+  }'
+
+echo
